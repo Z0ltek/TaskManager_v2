07fb83bb33628cda54776fef8c7e0fe859cbc054
Szymon <zoltowski.szymon@gmail.com>
2018-01-26 23:20:40 +0100

    poprawiona baza

--- TaskManager/profiles/models.py
+++ TaskManager/profiles/models.py
@@ -12,8 +12,7 @@ class Project(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.CharField(max_length=4000)
     last_updated = models.DateTimeField(auto_now_add=True)
-    owner = models.OneToOneField(User, related_name='project')
-    tasks = models.ForeignKey('Task', related_name='tasks_projects')
+    owner = models.ForeignKey(User, related_name='project')
 
     def __str__(self):
         return self.name
@@ -22,12 +21,12 @@ class Task(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=1000)
     status = EnumField(Status, max_length=1)
-    subtasks = models.ForeignKey('Subtask', related_name='subtasks_tasks')
+    project = models.ForeignKey(Project, related_name='tasks')
     time = models.DecimalField(max_digits=3, decimal_places=1, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
-    created_by = models.OneToOneField(User, related_name='tasks')
-    updated_by = models.OneToOneField(User, null=True, related_name='+')
+    created_by = models.ForeignKey(User, related_name='tasks')
+    updated_by = models.ForeignKey(User, null=True, related_name='+')
 
     def __str__(self):
         return self.name
@@ -36,13 +35,13 @@ class Subtask(models.Model):
     name = models.CharField(max_length=255)
     message = models.TextField(max_length=4000)
     status = EnumField(Status, max_length=1)
-    tasks = models.OneToOneField('Task', related_name='tasks_subtasks')
+    tasks = models.ForeignKey('Task', related_name='tasks_subtasks')
     time = models.DecimalField(max_digits=3, decimal_places=1, blank=True, null=True)
     last_updated = models.DateTimeField(auto_now_add=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(null=True)
-    created_by = models.OneToOneField(User, related_name='subtasks')
-    updated_by = models.OneToOneField(User, null=True, related_name='+')
+    created_by = models.ForeignKey(User, related_name='subtasks')
+    updated_by = models.ForeignKey(User, null=True, related_name='+')
 
     def __str__(self):
         return self.name
